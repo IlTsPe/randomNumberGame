0df7016a04618e5d46889a6279adca3ba5d8861d
IlTsPe <pegas19962010@gmail.com>
2023-04-30 16:33:07 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const input = document.querySelector('.number-input'),
+	check = document.querySelector('.check'),
+	again = document.querySelector('.again'),
+	guessMS = document.querySelector('.guess-message'),
+	question = document.querySelector('.question'),
+	scoreInner = document.querySelector('.score'),
+	highscore = document.querySelector('.highscore'),
+	body = document.querySelector('body');
+
+let score = 20;
+
+
+
+function generator() {
+	let number = Math.trunc((Math.random() * 20) + 1);
+	return number;
+}
+
+let secretNumber = generator();
+console.log(secretNumber);
+
+function setAttribute() {
+	question.style.width = '50rem';
+	check.setAttribute('disabled', '');
+	input.setAttribute('disabled', '');
+}
+
+function userWin() {
+	guessMS.textContent = 'Правильно!';
+	highscore.textContent = score;
+	question.textContent = input.value;
+	body.style.backgroundColor = 'rgb(24, 183, 32)';
+	setAttribute();
+}
+
+function criticalBalance() {
+	input.addEventListener('input', function () {
+		if (input.value > 20 || input.value <= Number(0)) {
+			this.style.color = 'red';
+			this.style.borderColor = 'red';
+			check.setAttribute('disabled', '');
+			question.textContent = 'от 1 до 20'
+		} else {
+			this.style.color = 'white';
+			this.style.borderColor = 'white';
+			check.removeAttribute('disabled', '');
+			question.textContent = '???'
+		}
+		this.value == '' ? (this.style.borderColor = 'white', this.style.color = 'white') : false;
+	})
+}
+
+criticalBalance();
+
+check.addEventListener('click', () => {
+	if (score > 1) {
+		input.style.borderColor = 'white'
+		input.value > secretNumber ? guessMS.textContent = 'Много!' : guessMS.textContent = 'Мало!';
+		input.value == secretNumber ? userWin() : score--;
+		scoreInner.textContent = score;
+	} else {
+		guessMS.textContent = 'Вы проиграли!';
+		scoreInner.textContent = '0';
+		question.textContent = secretNumber;
+		body.style.backgroundColor = 'rgb(181, 25, 25)'
+		setAttribute();
+	}
+});
+
+again.addEventListener('click', () => {
+	question.textContent = '???';
+	question.style.width = '25rem';
+	input.value = '';
+	check.removeAttribute('disabled');
+	input.removeAttribute('disabled', '');
+	guessMS.textContent = 'Начни угадывать';
+	scoreInner.textContent = '20';
+	body.style.backgroundColor = 'rgb(0, 0, 0)';
+	secretNumber = generator();
+	score = 20;
+	console.log(secretNumber);
+})
